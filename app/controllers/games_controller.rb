6f8f730ea7f44c6c8639da1393bca7d178f73a08
Alex Lizun <alexandrlizunm@gmail.com>
2019-10-21 18:28:46 +0300

    fix

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@
 
 class GamesController < ApplicationController
   ITEMS_PER_PAGE = 12
+  
   def index
     @q = Game.ransack(params[:q])
     @pagy, @games = pagy(@q.result, items: ITEMS_PER_PAGE)  
