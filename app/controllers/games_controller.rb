b2b1dd8738130513c1d74965a8e7088bb144e990
Alex Lizun <alexandrlizunm@gmail.com>
2019-10-21 18:23:03 +0300

    add bootstrap cards

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class GamesController < ApplicationController
-
+  ITEMS_PER_PAGE = 12
   def index
     @q = Game.ransack(params[:q])
-    @pagy, @games = pagy(@q.result, items: 9)  
+    @pagy, @games = pagy(@q.result, items: ITEMS_PER_PAGE)  
   end
 end
