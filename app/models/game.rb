e5a4d15ea4d3931fbfd68baeb2532859f02da717
Alex Lizun <alexandrlizunm@gmail.com>
2019-10-22 15:59:44 +0300

    add search by price and specs

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,7 @@ class Game < ApplicationRecord
   has_one_attached :cover
   validates :name, presence: true, length: { maximum: 128 }
   validates :price, presence: true, numericality: { greater_than: 0 }
+  ransacker :price_money, formatter: proc { |dollars| dollars.to_i * 100 } do |p|
+    p.table[:price]
+  end
 end
--- spec/features/games/search_game_spec.rb
+++ spec/features/games/search_game_spec.rb
@@ -6,16 +6,24 @@ require 'rails_helper'
 #   I want to search for game
 feature 'Search for Game', type: :feature do
   before do
-    create(:game, name: 'Portal')
-    create(:game, name: 'Fifa')
+    create(:game, name: 'Portal', price: 2000)
+    create(:game, name: 'Fifa', price: 5000)
   end
 
   # Scenario: I search for game successfully
   #   When I fill in a search form and press submit
   #   Then I have a results of search due to my form content
-  scenario 'Search for a game successfully', js: true do
+  scenario 'Search for a game successfully by name', js: true do
     visit games_path 
     fill_in 'q_name_cont', with: 'portal'
+    click_button 'search-button'
+    expect(page).not_to have_content 'Fifa'
+  end
+
+  scenario 'Search for a game successfully by price', js: true do
+    visit games_path 
+    fill_in 'q_price_money_gteq', with: 10
+    fill_in 'q_price_money_lteq', with: 30
     click_button 'search-button'
     expect(page).not_to have_content 'Fifa'
   end
