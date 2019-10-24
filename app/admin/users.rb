5aee87db24bb4cca17b0d38c89e51ce4d29fa526
Alex Lizun <alexandrlizunm@gmail.com>
2019-10-24 11:41:18 +0300

    add model to active admin

---
+++ app/admin/users.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register User do
+  permit_params :nickname, :email, :avatar
+
+  index do
+    selectable_column
+    id_column
+    column :nickname
+    column :email
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :nickname
+      f.input :email
+      f.input :avatar, as: :file
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :nickname
+      row :email
+      row :cover do |user|
+        image_tag(user.avatar, size: '300x200')
+      end
+    end
+  end
+end
