ea5d61d0d754756d8eaa64eac564c97b83bc94cd
AlexLizunM <alexandrlizunm@gmail.com>
2019-10-28 02:35:18 +0300

    working on user profile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require bootstrap-sprockets 
+//= require jquery
+//= require popper
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :games
+  resources :games, :users
   root to: 'games#index'
 end
