480ce3e0fb85e8d5e1299764ded4afd73490bf1a
Alex Lizun <alexandrlizunm@gmail.com>
2019-11-12 18:40:41 +0300

    secret base key

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module OnlineGamesStore
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.active_job.queue_adapter = :sidekiq
+    config.secret_key_base = ENV['SECRET_KEY_BASE']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
