5783133770889910f591277f44e55a55c0c44153
Alex Lizun <alexandrlizunm@gmail.com>
2019-11-13 15:50:36 +0300

    fix production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => 'gmail.com'
+  :domain               => 'gmail.com',
   :user_name            => ENV['MAIL_NAME'],
   :password             => ENV['MAIL_PASS'],
   :authentication       => "plain",
