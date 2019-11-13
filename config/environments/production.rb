62ef30f99da4d438931070a10684f819f1c5d95a
Alex Lizun <alexandrlizunm@gmail.com>
2019-11-13 15:21:10 +0300

    add mailer config for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,24 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.perform_deliveries = true
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: ENV['HOST_FB'] }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => ENV['MAIL_NAME'],
+  :password             => ENV['MAIL_PASS'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+  } 
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
